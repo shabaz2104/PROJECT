5dc78e70ce6c8ae2c5221eccdcf549aa85863c66
sshabaz210409@gmail.com <sshabaz210409@gmail.com>
2026-02-02 14:52:55 +0530

    stripped away the text based game and kept just the menu, now we add the 2D game touch

--- backend/app.py
+++ backend/app.py
@@ -1,34 +1,10 @@
-from flask import Flask, request, jsonify, render_template
-from logic.story import scene_one , opening_scene
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
     return render_template("index.html")
-
-
-@app.route("/scene/1", methods=["GET", "POST"])
-def handle_scene_1():
-
-    # 🔹 When the page loads (GET request)
-    if request.method == "GET":
-       return jsonify({
-        "scene": 1,
-        "result": opening_scene()
-    })
-
-    # 🔹 When the user makes a choice (POST request)
-    data = request.json
-    choice = data.get("choice")
-
-    result = scene_one(choice)
-
-    return jsonify({
-        "scene": 1,
-        "result": result
-    })
-
 
 if __name__ == "__main__":
     app.run(debug=True)
